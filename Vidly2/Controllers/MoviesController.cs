9eb6ad4f2b74b11b88765af54bf392cec1992f51
JawoszekR <jawoszekr@gmail.com>
2019-03-26 19:50:02 +0100

    Testing actionresults

--- Vidly2/Controllers/MoviesController.cs
+++ Vidly2/Controllers/MoviesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI.WebControls;
 using Vidly2.Models;
 
 namespace Vidly2.Controllers
@@ -13,7 +14,11 @@ namespace Vidly2.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
-            return View(movie);
+            //return View(movie);
+            //return Content("Hello World");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            //return RedirectToAction("Index", "Home", new { page =1, sortBy = "name"});
         }
     }
 }
