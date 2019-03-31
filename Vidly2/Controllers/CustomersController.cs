0fb6792b98a8b7d480372e583e1ab6c026b01376
JawoszekR <jawoszekr@gmail.com>
2019-03-31 14:55:42 +0200

    Exercise

---
+++ Vidly2/Controllers/CustomersController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly2.Models;
+
+namespace Vidly2.Controllers
+{
+    public class CustomersController : Controller
+    {
+        List<Customer> customersList = new List<Customer>()
+            {
+                new Customer{Name = "Rafał Jawoszek"},
+                new Customer{Name = "Jan Kowalski"}
+            };
+
+        [Route("Customers")]
+        public ActionResult Index()
+        {
+            return View(customersList);
+        }
+
+        [Route("Customers/Details/{Id}")]
+        public ActionResult Details(int Id)
+        {
+            return View(customersList[Id]);
+        }
+    }
+}
