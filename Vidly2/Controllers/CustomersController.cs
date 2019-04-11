616670d16493e4877ad59c72a46890ad85ae6fcc
JawoszekR <jawoszekr@gmail.com>
2019-04-11 18:44:09 +0200

    exercise

--- Vidly2/Controllers/CustomersController.cs
+++ Vidly2/Controllers/CustomersController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using Vidly2.Models;
 using System.Data.Entity;
+using Vidly2.ViewModels;
 
 namespace Vidly2.Controllers
 {
@@ -37,6 +38,55 @@ namespace Vidly2.Controllers
             var customer = _context.Customers.Include(m => m.MembershipType).SingleOrDefault(c => c.Id == Id);
 
             return View(customer);
+        }
+
+        public ActionResult New()
+        {
+            var membershipTypes = _context.MembershipTypes.ToList();
+            var viewModel = new CustomerFormViewModel
+            {
+                MembershipTypes = membershipTypes
+            };
+
+            return View("CustomerForm", viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Save(Customer customer)
+        {
+            if (customer.Id == 0)
+            {
+                _context.Customers.Add(customer);
+            }
+            else
+            {
+                var customerInDb = _context.Customers.Single(c => c.Id == customer.Id);
+                //TryUpdateModel(customerInDb);//uwaga aktualizuje wszystko co może być niebezpieczne
+                //TryValidateModel(customer, "", new string { "Name", "Birthdate"})//tylko wymienione
+                customerInDb.Name = customer.Name;
+                customerInDb.Birthdate = customer.Birthdate;
+                customerInDb.MembershipTypeId = customer.MembershipTypeId;
+                customerInDb.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+            }
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Customers");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            var viewModel = new CustomerFormViewModel
+            {
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes
+            };
+
+            return View("CustomerForm", viewModel);
         }
     }
 }
--- Vidly2/Controllers/MoviesController.cs
+++ Vidly2/Controllers/MoviesController.cs
@@ -44,11 +44,6 @@ namespace Vidly2.Controllers
             //return RedirectToAction("Index", "Home", new { page =1, sortBy = "name"});
         }
 
-        public ActionResult Edit(int id)
-        {
-            return Content("id=" + id);
-        }
-
         [Route("Movies")]
         public ActionResult Index()
         {
@@ -69,6 +64,55 @@ namespace Vidly2.Controllers
         public ActionResult ByReliseDate(int? year, int month)
         {
             return Content(year + "/" + month);
+        }
+
+        [HttpPost]
+        public ActionResult Save(Movie movie)
+        {
+            if (movie.Id == 0)
+            {
+                _context.Movies.Add(movie);
+            }
+            else
+            {
+                var movieInDb = _context.Movies.Single(c => c.Id == movie.Id);
+                //TryUpdateModel(movieInDb);//uwaga aktualizuje wszystko co może być niebezpieczne
+                //TryUpdateModel(movieInDb, "", new string[] { "Name", nameof(movieInDb.GenreID), nameof(movieInDb.NumberInStock), nameof(movieInDb.ReliseDate) });//tylko wymienione don't work ??
+                movieInDb.GenreID = movie.GenreID;
+                movieInDb.NumberInStock = movie.NumberInStock;
+                movieInDb.ReliseDate = movie.ReliseDate;
+                movieInDb.Name = movie.Name;
+            }
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Movies");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(c => c.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            var viewModel = new MovieFormViewModel
+            {
+                Movie = movie,
+                Genres = _context.Genres
+            };
+
+            return View("MovieForm", viewModel);
+        }
+
+        public ActionResult New()
+        {
+            var genres = _context.Genres.ToList();
+            var viewModel = new MovieFormViewModel
+            {
+                Genres = genres
+            };
+
+            return View("MovieForm", viewModel);
         }
     }
 }
