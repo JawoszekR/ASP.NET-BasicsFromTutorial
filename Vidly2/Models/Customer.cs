8b85b4885e002def575a9b6c654a5a701edd73f5
JawoszekR <jawoszekr@gmail.com>
2019-04-11 19:45:28 +0200

    Basic validation

--- Vidly2/Models/Customer.cs
+++ Vidly2/Models/Customer.cs
@@ -9,9 +9,11 @@ namespace Vidly2.Models
     public class Customer
     {
         public int Id { get; set; }
+
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
+
         public bool IsSubscribedToNewsletter { get; set; }
         public MembershipType MembershipType { get; set; }
         public byte MembershipTypeId { get; set; }
