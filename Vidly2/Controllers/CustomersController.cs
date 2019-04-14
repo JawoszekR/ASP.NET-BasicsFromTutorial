cf6f310ab4fcfb02a2fbd20ee21d7a055ef411a3
JawoszekR <jawoszekr@gmail.com>
2019-04-14 18:31:31 +0200

    Client-side Validation

--- Vidly2/Controllers/CustomersController.cs
+++ Vidly2/Controllers/CustomersController.cs
@@ -53,6 +53,7 @@ namespace Vidly2.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Save(Customer customer)
         {
             if (!ModelState.IsValid)
