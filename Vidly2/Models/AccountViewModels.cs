8da31c69addda6483e23cd9bb06884e803425013
JawoszekR <jawoszekr@gmail.com>
2019-05-26 13:36:31 +0200

    Exercise - phone number

--- Vidly2/Models/AccountViewModels.cs
+++ Vidly2/Models/AccountViewModels.cs
@@ -74,6 +74,12 @@ namespace Vidly2.Models
         public string Email { get; set; }
 
         [Required]
+        [Phone]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [Display(Name = "Phone number")]
+        public string Phone { get; set; }
+
+        [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
--- Vidly2/Models/IdentityModels.cs
+++ Vidly2/Models/IdentityModels.cs
@@ -13,6 +13,7 @@ namespace Vidly2.Models
         [Required]
         [StringLength(255)]
         public string DrivingLicense { get; set; }
+        public string PhoneNumber { get; set; }
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
